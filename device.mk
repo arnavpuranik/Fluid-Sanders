b1d1b3a6276fd94475bf74db89d5aa72de1a85fe
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    sanders: Use Pixel Charging Animation
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

--- device.mk
+++ device.mk
@@ -536,6 +536,11 @@ PRODUCT_PACKAGES += \
 #    QPGallery
 endif
 
+# Offline charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
