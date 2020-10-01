15a18243904be70814f1357ef87cfada0517bcd6
Lunarixus <Nathanb644@hotmail.co.uk>
2020-10-01 16:41:11 +0000

    sanders: Remove packages at build time
    * AOSP Screen recorder works fine enough.
    * @marshmello61 edit- add flag to enable it only if build with gapps
    
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- device.mk
+++ device.mk
@@ -521,6 +521,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gapps-config.txt:$(TARGET_COPY_OUT_VENDOR)/etc/gapps-config-sanders.txt
 
+ifeq ($(WITH_GAPPS), true)
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+endif
+
 ifneq ($(WITH_GAPPS), true)
 # Prebuilt Packages
 PRODUCT_PACKAGES += \
